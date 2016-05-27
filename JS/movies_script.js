9768039c44739a7f7a2e4bf34d440ef5d43b6e7b
Tomer Ben Kereth <bk.tomer@gmail.com>
2016-05-27 14:53:44 +0300

    autocomplete works

--- JS/movies_script.js
+++ JS/movies_script.js
@@ -1,16 +1,18 @@
-var app = angular.module('MoviesApp', []);
+var MoviesApp = angular.module('MoviesApp', ['ui.bootstrap']);
 
 
-app.controller('mainController', function($scope,$http) {
+MoviesApp.controller('mainController', function($scope,$http) {
 
-    $scope.selected = undefined;
     $scope.actors  = [
-                        {name:"dan", year:1995},
-                        {name:"tomer",year:1990},
-                        {name:"dror",year:1780}];
+                        {name:"dan", year:1995, country: "Israel"},
+                        {name:"tomer",year:1990, country: "USA"},
+                        {name:"dror",year:1780, country: "UK"}
+    ];
 
 
-
+    $scope.ActorSelected = function(){
+        return !($scope.selected_actor === undefined);
+    }
 
 });
 

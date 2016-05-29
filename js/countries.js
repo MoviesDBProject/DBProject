8b3bf574934255441933196733353d7d744baaf8
Tomer Ben Kereth <bk.tomer@gmail.com>
2016-05-29 18:26:35 +0300

    start of cssing

---
+++ js/countries.js
@@ -0,0 +1,27 @@
+/*
+	*	Original script by: Shafiul Azam
+	*	ishafiul@gmail.com
+	*	Version 3.0
+	*	Modified by: Luigi Balzano
+
+	*	Description:
+	*	Inserts Countries and/or States as Dropdown List
+	*	How to Use:
+
+		In Head section:
+		<script type= "text/javascript" src = "countries.js"></script>
+		In Body Section:
+		Select Country:   <select onchange="print_state('state',this.selectedIndex);" id="country" name ="country"></select>
+		<br />
+		City/District/State: <select name ="state" id ="state"></select>
+		<script language="javascript">print_country("country");</script>	
+
+	*
+	*	License: OpenSource, Permission for modificatin Granted, KEEP AUTHOR INFORMATION INTACT
+	*	Aurthor's Website: http://shafiul.progmaatic.com
+	*
+*/
+
+
+
+

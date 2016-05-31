238eaac9382462b727a883e83edc17dbe80ba4eb
Tomer Ben Kereth <bk.tomer@gmail.com>
2016-05-31 21:57:28 +0300

    trying to render html

--- moviestats/views.py
+++ moviestats/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {}
+    return render(request, 'moviestats/index.html', context)
+
+#    return HttpResponse("Hello, world. You're at the polls index.")

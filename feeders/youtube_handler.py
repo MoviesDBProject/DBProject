d16971cd2c4e6335077f11fc60b4fc83de30ca0a
mostafawattad <wattad169@gmail.com>
2016-06-01 20:12:05 +0300

    youtube fetcher summary

--- feeders/youtube_handler.py
+++ feeders/youtube_handler.py
@@ -26,7 +26,7 @@ def youtube_search(search_term):
 	search_response = youtube.search().list(
 			q=search_term,
 			part="id,snippet",
-			maxResults=25
+			maxResults=1
 	).execute()
 
 	videos = []
@@ -35,16 +35,17 @@ def youtube_search(search_term):
 
 	# Add each result to the appropriate list, and then display the lists of
 	# matching videos, channels, and playlists.
-	for search_result in search_response.get("items", []):
-		if search_result["id"]["kind"] == "youtube#video":
-			videos.append("%s (%s)" % (search_result["snippet"]["title"],
-			                           search_result["id"]["videoId"]))
-		elif search_result["id"]["kind"] == "youtube#channel":
-			channels.append("%s (%s)" % (search_result["snippet"]["title"],
-			                             search_result["id"]["channelId"]))
-		elif search_result["id"]["kind"] == "youtube#playlist":
-			playlists.append("%s (%s)" % (search_result["snippet"]["title"],
-			                              search_result["id"]["playlistId"]))
+	print search_response.get("items", [])[0]
+	# for search_result in search_response.get("items", []):
+	# 	if search_result["id"]["kind"] == "youtube#video":
+	# 		videos.append("%s (%s)" % (search_result["snippet"]["title"],
+	# 		                           search_result["id"]["videoId"]))
+	# 	elif search_result["id"]["kind"] == "youtube#channel":
+	# 		channels.append("%s (%s)" % (search_result["snippet"]["title"],
+	# 		                             search_result["id"]["channelId"]))
+	# 	elif search_result["id"]["kind"] == "youtube#playlist":
+	# 		playlists.append("%s (%s)" % (search_result["snippet"]["title"],
+	# 		                              search_result["id"]["playlistId"]))
 
 	print "Videos:\n", "\n".join(videos), "\n"
 	print "Channels:\n", "\n".join(channels), "\n"

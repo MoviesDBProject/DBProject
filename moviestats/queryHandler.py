9cbd2d208988e13ee80658b6b8938e9620a39f08
mostafawattad <wattad169@gmail.com>
2016-06-01 10:43:30 +0300

    added js

--- moviestats/queryHandler.py
+++ moviestats/queryHandler.py
@@ -1,5 +1,17 @@
-
 from django.http import HttpResponse
+import mysql.connector
 
 def handle_query(request):
+	example_json = {"actor": "dan",
+	                "actor_birth_date": 1963,
+	                "actor_birth_place": "USA",
+	                "director": "tomer",
+	                "film_location": "algeria",
+	                "film_language": "arabic",
+	                "min_views": "200000",
+	                "sort":True
+}
+
+
+
 	return HttpResponse("test")

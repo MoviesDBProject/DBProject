0492c12668436c22808dcaad38a10004227c95ab
mostafawattad <wattad169@gmail.com>
2016-06-05 20:08:12 +0300

    new dbpedia feeder

--- feeders/constatns.py
+++ feeders/constatns.py
@@ -1,6 +1,21 @@
 from rdflib import Graph, URIRef, RDF
 
-DIRECTOR = "director"
+# scheme
+# movie_info = {
+# 					ACTORS : actors(LIST),
+# 					DIRECTORS : directors(LIST),
+# 					COUNTRY : country(STR),
+# 					NAME : film_name(STR),
+# 					ID : film_id(STR)
+# 				}
+
+
+
+
+
+
+
+DIRECTORS = "directors"
 TITLE = 'title'
 ACTORS = 'actors'
 LANGUAGE = 'language'
@@ -13,21 +28,32 @@ NAME = "name"
 DBPEDIA_PREFIX = "http://dbpedia"
 YOUTUBE = "youtube"
 WIKI_ID = 'wiki_id'
-
-ITERS = 1
-RANGE = 100
+ID = 'id'
+NA = 'N/A'
+ITERS = 10
+RANGE = 1000
 
 movieMapFromRdf = {
 
-    "http://dbpedia.org/ontology/director": DIRECTOR,
-    "http://dbpedia.org/property/starring": ACTORS,
-    "http://xmlns.com/foaf/0.1/name": TITLE,
-    "http://dbpedia.org/property/language": LANGUAGE,
-    "http://dbpedia.org/property/country": COUNTRY
+	"http://dbpedia.org/ontology/director": DIRECTORS,
+	"http://dbpedia.org/property/starring": ACTORS,
+	"http://xmlns.com/foaf/0.1/name": TITLE,
+	"http://dbpedia.org/property/language": LANGUAGE,
+	"http://dbpedia.org/property/country": COUNTRY
 }
 
 personMapFromRdf = {
-    "http://dbpedia.org/property/placeOfBirth": BIRTH_PLACE,
-    "http://dbpedia.org/ontology/birthDate": BIRTH_DATE,
-    "http://xmlns.com/foaf/0.1/name": NAME
+	"http://dbpedia.org/property/placeOfBirth": BIRTH_PLACE,
+	"http://dbpedia.org/ontology/birthDate": BIRTH_DATE,
+	"http://xmlns.com/foaf/0.1/name": NAME
 }
+
+
+# OMDB constans
+
+OMDB_COUNTRY = 'Country'
+OMDB_LANGUAGE = 'Language'
+OMDB_GENRE = 'Genre'
+OMDB_IMDB_RATING = 'imdbRating'
+OMDB_YEAR = 'Year'
+OMDB_RUNTIME = 'Runtime'
--- feeders/dbpedia_feeder_new.py
+++ feeders/dbpedia_feeder_new.py
@@ -2,25 +2,37 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 from rdflib import Graph, URIRef, RDF
 from constatns import *
 from youtube_handler import *
-import urllib2,json
-
+import urllib3
+import json
+import pprint
+import sys
 counter = 0
 
 # Movie id => movie url dbpedia
 # Actor id => if has page ==> page link dbpedia, else id is his name
 # The same for director
+actors_to_fetch = []
+directors_to_fetch = []
+actors_to_insert = []
+directors_to_insert = []
+
 
 def get_movies_from_dbpedia():
-    films = set()
-    films_info = []
-    for i in range(ITERS):
-        start = i * RANGE
-        end = (i + 1) * RANGE
-        sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+	global actors_to_fetch
+	global directors_to_fetch
+	global actors_to_insert
+	global directors_to_insert
+	films = set()
+	films_info = []
+	fetched = 0
+	for i in range(ITERS):
+		start = i * RANGE
+		end = (i + 1) * RANGE
+		sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 
-        sparql.setQuery("""
+		sparql.setQuery("""
             PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-            SELECT DISTINCT ?film_name
+            SELECT DISTINCT ?film_name ?film_title
             GROUP_CONCAT(DISTINCT ?film_actors ; SEPARATOR = "|")
             GROUP_CONCAT(DISTINCT ?film_director ; SEPARATOR = "|")
             ?film_country
@@ -34,153 +46,173 @@ def get_movies_from_dbpedia():
                    ?film_title dbp:director ?film_director .
                 }
                 optional{
-                   ?film_title dbp:country ?film_country
+                   ?film_title dbp:country ?film_country .
                 }
+
 
                 FILTER (langMatches(lang(?film_name),"en"))
 
 
             }LIMIT %s OFFSET %s
             """ % (end, start))
-        print("[+]==== Getting movies from %d to %d ====[+]" %(start, end))
-        sparql.setReturnFormat(JSON)
-        results = sparql.query().convert()
-        fetched= 0
-        # iterate over films
-        for film in results["results"]["bindings"]:
-            try:
-                film_id = film['film_title']['value']
-                if film_id not in films:
-                    fetched += 1
-                    try:
-                        movie_info = fetch_movie_info(film_id)
-                        movie_info[ABSTRACT] = film['film_abstract']['value']
-                        # movie_info[WIKI_ID] = get_wiki_id(movie_info[TITLE])
-                        films_info.append(movie_info)
-                        films.add(film_id)
-                        print movie_info
-                        print fetched
-                    except:
-                        pass
-                        # print "Success score: {0}/{1}".format(counter,fetched)
+		print("[+]==== Getting movies from %d to %d ====[+]" % (start, end))
+		sparql.setReturnFormat(JSON)
+		results = sparql.query().convert()
 
-            except Exception as e:
-                print "Failed, Breaking"
-                print str(e)
-                break
+		# iterate over films
+		for film in results["results"]["bindings"]:
+			try:
+				film_id = str(film.get('film_title', {}).get('value', ''))
+				if film_id not in films:
 
-    print("[+]==== Fetched %d movies from DBPedia ====[+]" % len(films))
-    return films_info
+					try:
+						movie_info = fetch_movie_info(film)
+						films_info.append(movie_info)
+						films.add(film_id)
+						fetched += 1
+						pprint.pprint(movie_info)
+						print fetched
+					except Exception as e:
+						print str(e)
+						pass
+					# print "Success score: {0}/{1}".format(counter,fetched)
 
+			except Exception as e:
+				# print "Failed, Breaking"
+				print str(e)
+				continue
+			# print("[+]==== Getting actors info ====[+]")
+			# actors_to_insert = fetch_person_info(actors_to_fetch)
+			# print("[+]==== Getting directors info ====[+]")
+			# directors_to_insert = fetch_person_info(directors_to_fetch)
 
-def fetch_movie_info(url):
-    global counter
-    success = True
-    result = {}
-    uri = URIRef(url)
-    graph = Graph()
-    graph.parse(uri)
-
-    for s, parent, offspring in graph:
-        try:
-            rdf_key_str = str(parent)  # rdf property
-            rdf_val_str = str(offspring)   # value
-
-            # check if the each of the film rdf dependencies' type is one of those we want to save
-            if rdf_key_str in movieMapFromRdf and rdf_val_str != '*':
-                key_in_result = movieMapFromRdf[rdf_key_str]
-                if key_in_result == ACTORS:
-                    if key_in_result in result:
-                        result[key_in_result].append(rdf_val_str)
-                    else:
-                        key_in_result = movieMapFromRdf[rdf_key_str]  # is this line really needed?
-                        result[key_in_result] = [rdf_val_str]
-                else:
-                    result[key_in_result] = rdf_val_str
-        except:
-            pass
-
-    if ACTORS in result:
-        result[ACTORS] = fetch_person_info(result[ACTORS])
-    else:
-        result[ACTORS] = []
-
-    if DIRECTOR in result:
-        result[DIRECTOR] = fetch_person_info([result[DIRECTOR]])[0]
-    else:
-        result[DIRECTOR] = []
-
-    if LANGUAGE in result and result[LANGUAGE].startswith(DBPEDIA_PREFIX):
-        result[LANGUAGE] = fetch_name_from_dbpedia_page(result[LANGUAGE])
-
-    if COUNTRY in result and result[COUNTRY].startswith(DBPEDIA_PREFIX):
-        result[COUNTRY] = fetch_name_from_dbpedia_page(result[COUNTRY])
-
-    if success:
-        counter += 1
-    try:
-        result[YOUTUBE] = youtube_search(result[TITLE] + " trailer")
-    except:
-        pass
-
-    print("==============================================")
-    return result
+	print("[+]==== Fetched %d movies from DBPedia ====[+]" % len(films))
+	return films_info
 
 
-def fetch_person_info(persons_urls):
-    result = []
-    for person_url in persons_urls:
-        if not person_url.startswith(DBPEDIA_PREFIX):
-            return {NAME: person_url}
+def fetch_movie_info(film):
+	global counter
+	global actors_to_insert
+	global directors_to_insert
+	success = True
+	movie_info = {}
 
-        try:
-            person_info = {}
-            uri = URIRef(person_url)
-            graph = Graph()
-            graph.parse(uri)
-            for s, p, o in graph:
-                try:
-                    rdf_key_str = str(p)
-                    rdf_val_str = str(o)
-                    # print "{0} : {1}".format(rdf_key_str,rdf_val_str)
-                    if rdf_key_str in personMapFromRdf:
-                        person_info[personMapFromRdf[rdf_key_str]] = rdf_val_str
-                except:
-                    pass
-            if BIRTH_PLACE in person_info and person_info[BIRTH_PLACE].startswith(DBPEDIA_PREFIX):
-                person_info[BIRTH_PLACE] = fetch_name_from_dbpedia_page(person_info[BIRTH_PLACE])
+	try:
+		film_id = str(film.get('film_title', {}).get('value', ''))
+		actors = film.get('callret-2', {}).get('value', '').split("|")
+		if "*" in actors:
+			actors.remove("*")
 
-            # person_info[WIKI_ID] = get_wiki_id(person_info[NAME])
-            result.append(person_info)
-        except:
-            pass
+		directors = film.get('callret-3', {}).get('value', '').split("|")
+		if "*" in actors:
+			directors.remove("*")
 
-    return result
+		country = film.get('film_country', {}).get('value', NA)
+		film_name = film.get('film_name', {}).get('value', NA)
+		movie_info = {
+			ACTORS: actors,
+			DIRECTORS: directors,
+			COUNTRY: country,
+			TITLE: film_name,
+			ID: hash(film_id) % ((sys.maxsize + 1) * 2)
+		}
+	except Exception as e:
+		print str(e)
+
+	for person in movie_info[ACTORS]:
+		actors_to_insert.append(fetch_person_info(person))
+
+	for person in movie_info[DIRECTORS]:
+		directors_to_insert.append(fetch_person_info(person))
+
+	try:
+		omdb_result = get_omdb(film_name)
+		movie_info[COUNTRY] = omdb_result.get(OMDB_COUNTRY,NA)
+		movie_info[OMDB_IMDB_RATING.lower()] = omdb_result.get(OMDB_IMDB_RATING,NA)
+		movie_info[OMDB_LANGUAGE.lower()] = omdb_result.get(OMDB_LANGUAGE,NA)
+		movie_info[OMDB_RUNTIME.lower()] = omdb_result.get(OMDB_RUNTIME,NA)
+		imdb_genre = omdb_result.get(OMDB_GENRE,NA)
+		if imdb_genre !=NA:
+			imdb_genre = imdb_genre.split(',')
+			for i in imdb_genre:
+				i.replace(" ","")
+		else:
+			imdb_genre = [NA]
+		movie_info[OMDB_GENRE.lower()] =imdb_genre
+		movie_info[OMDB_YEAR.lower()] = omdb_result.get(OMDB_YEAR,NA)
+	except Exception as e:
+		print str(e)
+		pass
+
+	if success:
+		counter += 1
+	try:
+		movie_info[YOUTUBE] = youtube_search(movie_info[TITLE] + " trailer")
+	except:
+		pass
+
+	print("==============================================")
+	return movie_info
 
 
-def get_wiki_id(name):
-    name = name.replace(" ", "_")
-    wiki_id = urllib2.urlopen('https://en.wikipedia.org/w/api.php?action=query&titles=%s&format=json' % name)
-    json_id = json.load(wiki_id)
-    return json_id['query']['pages'].keys()[0]
+def get_omdb(film_title):
+	link_omdb = "http://www.omdbapi.com/?t={0}&plot=short&r=json".format(film_title.replace(" ","+"))
+	index_of = film_title.find("(")
+	if index_of:
+		# if the film title contains parenthesis and a year, query OMDB with that year to avoid movies with the same name
+		year_part = film_title[film_title.find("(")+1:]
+		integers_in_year_part = [int(s) for s in year_part.split(" ") if s.isdigit()]
+		film_title = film_title[:film_title.find("(") - 1]
+		if len(integers_in_year_part) >0:
+			year = integers_in_year_part[0]
+			link_omdb = "http://www.omdbapi.com/?t={0}&y={1}&plot=short&r=json".format(film_title.replace(" ","+"),year)
+
+	http = urllib3.PoolManager()
+	r = http.request('GET', link_omdb)
+	if r.status !=200:
+		return {}
+	result = json.loads(r.data)
+	return result
+
+
+def fetch_person_info(person_dbpedia):
+	result = []
+	count = 0
+	person_info = {}
+	if person_dbpedia.startswith(DBPEDIA_PREFIX):
+		id = person_dbpedia
+		name = person_dbpedia[person_dbpedia.rfind("/") + 1:].replace("_", " ")
 
 
 
+	else:
+		name = person_dbpedia
+		id = name
+
+	person_info = {
+		NAME: name,
+		ID: hash(id) % ((sys.maxsize + 1) * 2)
+	}
+
+	# pprint.pprint(person_info)
+	# print "====================================="
+
+	return person_info
 
 
 def fetch_name_from_dbpedia_page(url):
-    uri = URIRef(url)
-    graph = Graph()
-    graph.parse(uri)
-    for s, p, o in graph:
-        try:
-            rdf_key_str = str(p)
-            rdf_val_str = str(o)
-            if rdf_key_str in personMapFromRdf and personMapFromRdf[rdf_key_str] == NAME:
-                return rdf_val_str
-        except:
-            pass
+	uri = URIRef(url)
+	graph = Graph()
+	graph.parse(uri)
+	for s, p, o in graph:
+		try:
+			rdf_key_str = str(p)
+			rdf_val_str = str(o)
+			if rdf_key_str in personMapFromRdf and personMapFromRdf[rdf_key_str] == NAME:
+				return rdf_val_str
+		except:
+			pass
 
 
 if __name__ == '__main__':
-    get_movies_from_dbpedia()
+	get_movies_from_dbpedia()

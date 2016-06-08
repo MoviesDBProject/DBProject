9514b8a23f754150555e02fd7c09d4043473a324
Calus <drorsu@gmail.com>
2016-06-08 14:36:23 +0300

    Added the query handling system

--- moviestats/queryHandler.py
+++ moviestats/queryHandler.py
@@ -73,9 +73,12 @@ def handle_query(request):
 
     query = ""
 
+    # Top actors
     if request_array['top_actors'] == True :
-        query = "SELECT youtube_id, title, rating, name
-                FROM top_actors ORDER BY rating DESC LIMIT 10"
+        query = """SELECT youtube_id, title, rating, name
+                FROM top_actors
+                ORDER BY rating DESC
+                LIMIT 10"""
 
         cursor.execute(query)
 
@@ -83,23 +86,76 @@ def handle_query(request):
 
         return HttpResponse(json.dumps(rows), content_type="application/json")
 
-    example_json = {"actor": "dan",
-                    "actor_birth_date": 1963,
-                    "actor_birth_place": "USA",
-                    "director": "tomer",
-                    "film_location": "algeria",
-                    "film_language": "arabic",
-                    "min_views": "200000",
-                    "sort": True
-                    }
-    cursor.execute(""" SELECT movie_url FROM trailers
- 				       WHERE trailers.views_count > {0} JOIN
-						(SELECT actors.movie_id as sub_movie_id FROM actors
-						 WHERE actors.first_name = {1}
-						JOIN movie_actor ON actors.actor_id = movie_actor.actor_id) as sub_query
-					   ON trailers.movie_id = sub_query.sub_movie_id;
-	                """.format(example_json["min_views"], example_json["actor"]))
-    row = cursor.fetchall()
+    # Creating query with search paramter
+    query = "SELECT selected_movie.title, selected_movie.rating, selected_movie.actor, selected_movie.director,
+            selected_movie.genre, selected_movie.country, selected_movie.language, trailers.youtube_id, trailers.view_count, "
 
+    if request_array['max_likes'] == True :
+        query += "MAX(trailers.likes) "
+    else :
+        query += "trailers.likes "
+
+    query += "FROM trailers JOIN (SELECT movies.title, movies.rating, selected_actor.actor, selected_director.director,
+                selected_genre.genre, selected_country.country, selected_language.language, movies.movie_id FROM movies
+                JOIN (SELECT actors.name AS actor, movie_actor.movie_id
+                FROM actors JOIN movie_actor ON actors.actor_id = movie_actor.actor_id "
+
+    if request_array['actor'] != None :
+        add_to_query = "WHERE actors.name = '{}'".format(request_array['actor'])
+        query += add_to_query
+
+    query += ") AS selected_actor ON movies.movie_id = selected_actor.movie_id
+                JOIN (SELECT directors.name AS director, movie_director.movie_id
+                FROM directors JOIN movie_director ON directors.director_id = movie_director.director_id "
+
+    if request_array['director'] != None :
+        add_to_query = "WHERE directors.name = '{}'".format(request_array['director'])
+        query += add_to_query
+
+    query += ") AS selected_director ON movies.movie_id = selected_director.movie_id
+                JOIN (SELECT genres.genre, movie_genre.movie_id
+              	FROM genres JOIN movie_genre ON genres.genre_id = movie_genre.genre_id "
+
+    if request_array['film_genre'] != None :
+        add_to_query = "WHERE genres.genre = '{}'".format(request_array['film_genre'])
+        query += add_to_query
+
+    query += ") AS selected_genre ON movies.movie_id = selected_genre.movie_id
+                JOIN (SELECT country.country_id, country.country
+              	FROM country "
+
+    if request_array['film_location'] != None :
+        add_to_query = "WHERE country.country = '{}'".format(request_array['film_location'])
+        query += add_to_query
+
+    query += ") AS selected_country ON selected_country.country_id = movies.country_id
+                JOIN (SELECT language.language_id, language.language
+                FROM language "
+
+    if request_array['film_language'] != None :
+        add_to_query = "WHERE language.language = '{}'".format(request_array['film_language'])
+        query += add_to_query
+
+    query += ") AS selected_language ON selected_language.language_id = movies.language_id "
+
+    if request_array['rating'] != None :
+        add_to_query = "WHERE movies.rating > {}".format(request_array['rating'])
+        query += add_to_query
+
+    query += ") AS selected_movie ON trailers.movie_id = selected_movie.movie_id "
+
+    if request_array['min_views'] != None :
+        add_to_query = "WHERE trailers.view_count > {} ".format(request_array['min_views'])
+        query += add_to_query
+
+    if request_array['max_likes'] == True :
+        query += ";"
+    else :
+        query += "GROUP BY selected_movie.movie_id;"
+
+
+    cursor.execute(query)
+
+    rows = dictFetchall(cursor)
 
     return HttpResponse(json.dumps(rows), content_type="application/json")
